60f1c30433466598fb8a5cc4f8d004522068128f
IrynaKyslytsia <114502274+IrynaKyslytsia@users.noreply.github.com>
2023-06-24 21:20:17 +0300

    Add Login & Register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,57 @@
-
+import { useState } from 'react';
+// import { useDispatch } from 'react-redux';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    // const dispatch = useDispatch();
+
+    const handleChange = ({ target: { name, value } }) => {
+        switch (name) {
+            case 'email':
+            return setEmail(value);
+        case 'password':
+            return setPassword(value);
+        default:
+            return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // dispatch(authOperations.logIn({ email, password }));
+    setEmail('');
+    setPassword('');
+  };
+
     return (
-        <div>Login</div>
+        <div>
+            <h1>Log in</h1>
+
+            <form onSubmit={handleSubmit} autoComplete="off">
+                <label>
+                    Email
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <button type="submit">Log in</button>
+            </form>
+        </div>
     )
 };
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,71 @@
+import { useState } from "react";
+// import { useDispatch } from "react-redux";
 
 
 const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    // const dispatch = useDispatch();
+
+    const handleChange = ({ target: { name, value } }) => {
+        switch (name) {
+            case 'name':
+        return setName(value);
+            case 'email':
+        return setEmail(value);
+            case 'password':
+        return setPassword(value);
+            default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // dispatch(authOperations.register({ name, email, password }));
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
     return (
-        <div>Register</div>
+        <div>
+            <h1>Sign up</h1>
+
+            <form onSubmit={handleSubmit} autoComplete="off">
+                <label>
+                    Name
+                    <input 
+                        type="text" 
+                        name="name" 
+                        value={name} 
+                        onChange={handleChange} />
+                </label>
+
+                <label>
+                    Email
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <button type="submit">Sign up</button>
+            </form>
+    </div>
     )
 };
 
