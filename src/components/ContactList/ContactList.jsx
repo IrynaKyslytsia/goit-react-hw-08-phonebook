e9230f4ff5cda079fab52066e68360a5e01f44f8
IrynaKyslytsia <114502274+IrynaKyslytsia@users.noreply.github.com>
2023-06-27 21:23:32 +0300

    Fixed Contacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,8 +1,6 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import ContactListItem from "components/ContactListItem/ContactListItem";
 import { selectContacts, selectFilter } from "redux/contacts/selectors";
-import { getContacts } from "redux/contacts/operations";
 import css from './ContactList.module.css'
 
 const getFilteredContacts = (contacts, filter) => {
@@ -15,13 +13,9 @@ const ContactList = () => {
     
     const contacts = useSelector(selectContacts);
     const filter = useSelector(selectFilter);
-    const dispatch = useDispatch();
+  
     const visibleContacts = getFilteredContacts(contacts, filter);
     
-    useEffect(() => {
-      dispatch(getContacts())
-  }, [dispatch])
-
     return (
         <ul className={css.list}>
             {visibleContacts.map(({ name, number, id }) => (
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectContacts } from "redux/contacts/selectors";
 import ContactForm from "components/ContactForm/ContactForm";
 import ContactList from "components/ContactList/ContactList";
 import Filter from "components/Filter/Filter";
 import Modal from "components/Modal/Modal";
 import AddButton from "components/AddButton/AddButton";
+import { getContacts } from "redux/contacts/operations";
 
 const Contacts = () => {
 
     const contacts = useSelector(selectContacts);
+    const dispatch = useDispatch();
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const toggleModal = () => setIsModalOpen(state => !state);
+
+    useEffect(() => {
+        dispatch(getContacts())
+    }, [dispatch])
 
     return (
         <>
